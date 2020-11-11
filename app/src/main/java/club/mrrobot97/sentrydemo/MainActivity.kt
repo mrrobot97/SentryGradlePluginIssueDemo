83abd741367bc24e3f887dda756c7036acbbf0a7
hiki <yang199714@gmail.com>
2020-11-11 17:21:50 +0800

    first commit

---
+++ app/src/main/java/club/mrrobot97/sentrydemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package club.mrrobot97.sentrydemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
